b2ff322aa99aaacfb15b5404175475b08678c0f2
lochnan <56927193+lochnan@users.noreply.github.com>
2019-11-20 09:15:49 -0600

    20 Nov Nancy Motrar Grass texto e imagen

--- src/main.js
+++ src/main.js
@@ -16,39 +16,47 @@ function screenGrass() {
     document.getElementById("main").style.display = "none";
     document.getElementById("grass").style.display = "block";
 
-/*CreateElment con un for para crear de forma dinámica los links 
-de los pokemones según la categoría de Grass  */
- for (let i=0; i<pokemonsByType.length; i++){
- let imgDiv = document.createElement('img');
- imgDiv.setAttribute ('class', 'democlass');
- imgDiv.setAttribute ('src', pokemonsByType[i].img);
- document.getElementById('categoryGrass').appendChild(imgDiv);
+    /*CreateElment con un for para crear de forma dinámica los links 
+    de los pokemones según la categoría de Grass  */
+    for (let i = 0; i < pokemonsByType.length; i++) {
+        let cardDiv = document.createElement('div') 
+        let imgDiv = document.createElement('img');
+        let txtDiv = document.createElement('span');
+        cardDiv.appendChild(imgDiv);
+        cardDiv.appendChild(txtDiv);
 
-// Creando un nodo hijo para el texto y asignando como padre a imgDiV
+        imgDiv.setAttribute('class', 'democlass');
+        imgDiv.setAttribute('src', pokemonsByType[i].img);
+        cardDiv.setAttribute('class', 'cardClass');
+        txtDiv.innerHTML = `<p>${pokemonsByType[i].name}</p>`;
+        //txtDiv.setAttribute('value', pokemonsByType[i].name);
 
+        document.getElementById('categoryGrass').appendChild(cardDiv);
 
- }; 
+        // Creando un nodo hijo para el texto y asignando como padre a imgDiV
 
- let resultByType = pokemonsByType;
+    };
+
+    let resultByType = pokemonsByType;
     console.log(resultByType);
 
-}; 
+};
 
 
 
         // Creando un elemento en el DOM para las imágenes.
-        /*
-        let imgTestDiv = document.createElement('img');
-        imgTestDiv.src = 'http://www.serebii.net/pokemongo/pokemon/060.png';
-        document.getElementById('categoryGrass').appendChild(imgTestDiv);
+/*
+let imgTestDiv = document.createElement('img');
+imgTestDiv.src = 'http://www.serebii.net/pokemongo/pokemon/060.png';
+document.getElementById('categoryGrass').appendChild(imgTestDiv);
 
-        let contentNameDiv = document.createElement("div");
-        let contentName = document.createTextNode("Aquí va el nombre del pokemon");
-        contentNameDiv.appendChild(contentName);
-        // añadiendo al elmento creado y su contenido al DOM Revisar https://developer.mozilla.org/es/docs/Web/API/Document/createElement
-        let currentDivName = document.getElementById('imgTestDiv');
-        document.body.insertBefore(contentNameDiv, currentDivName);
-        */
+let contentNameDiv = document.createElement("div");
+let contentName = document.createTextNode("Aquí va el nombre del pokemon");
+contentNameDiv.appendChild(contentName);
+// añadiendo al elmento creado y su contenido al DOM Revisar https://developer.mozilla.org/es/docs/Web/API/Document/createElement
+let currentDivName = document.getElementById('imgTestDiv');
+document.body.insertBefore(contentNameDiv, currentDivName);
+*/
 
  // Creando un elemento en el DOM para las imágenes.
 
