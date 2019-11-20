c420ebd74ca94d3e32c9e4aaa190505cfeeee0e5
lochnan <56927193+lochnan@users.noreply.github.com>
2019-11-19 23:09:42 -0600

    19 nov Pokemones imagenes mostrandose Nancy

--- src/main.js
+++ src/main.js
@@ -4,12 +4,9 @@
  * import POTTER from './data/potter/potter.js'
  */
 
-
 import { pokemon } from './data/pokemon/pokemon.js'; //poner el typemodule//
 import { pokemonsByType } from './data.js';
 import { typeStringPokemons } from './data.js';
-
-
 
 //Botones para abrir las  categorías
 document.getElementById("Bgrass").addEventListener("click", screenGrass);
@@ -19,13 +16,28 @@ function screenGrass() {
     document.getElementById("main").style.display = "none";
     document.getElementById("grass").style.display = "block";
 
-    let resultByType = pokemonsByType;
+/*CreateElment con un for para crear de forma dinámica los links 
+de los pokemones según la categoría de Grass  */
+ for (let i=0; i<pokemonsByType.length; i++){
+ let imgDiv = document.createElement('img');
+ imgDiv.setAttribute ('class', 'democlass');
+ imgDiv.setAttribute ('src', pokemonsByType[i].img);
+ document.getElementById('categoryGrass').appendChild(imgDiv);
+
+// Creando un nodo hijo para el texto y asignando como padre a imgDiV
+
+
+ }; 
+
+ let resultByType = pokemonsByType;
     console.log(resultByType);
 
+}; 
 
-    let totalPokemons = 0;
-  
+
+
         // Creando un elemento en el DOM para las imágenes.
+        /*
         let imgTestDiv = document.createElement('img');
         imgTestDiv.src = 'http://www.serebii.net/pokemongo/pokemon/060.png';
         document.getElementById('categoryGrass').appendChild(imgTestDiv);
@@ -36,4 +48,10 @@ function screenGrass() {
         // añadiendo al elmento creado y su contenido al DOM Revisar https://developer.mozilla.org/es/docs/Web/API/Document/createElement
         let currentDivName = document.getElementById('imgTestDiv');
         document.body.insertBefore(contentNameDiv, currentDivName);
-};
+        */
+
+ // Creando un elemento en el DOM para las imágenes.
+
+
+
+
